9ad64099d82dbca813fb5bcab142e1649eec9478
Thomas Pelletier <pelletier.thomas@gmail.com>
2017-02-03 15:14:14 -0800

    Ruby 2.4 compatibility for integer constraint

--- lib/stronger_parameters/constraints/integer_constraint.rb
+++ lib/stronger_parameters/constraints/integer_constraint.rb
@@ -3,10 +3,14 @@ require 'stronger_parameters/constraints'
 module StrongerParameters
   class IntegerConstraint < Constraint
     def value(v)
-      if v.is_a?(Fixnum) || v.is_a?(Bignum)
-        return v
-      elsif v.is_a?(String) && v =~ /\A-?\d+\Z/
+      if v.is_a?(String) && v =~ /\A-?\d+\Z/
         return v.to_i
+      else
+        if 0.class == Integer # ruby 2.4
+          return v if v.is_a?(Integer)
+        else # ruby < 2.4
+          return v if  v.is_a?(Fixnum) || v.is_a?(Bignum)
+        end
       end
 
       InvalidValue.new(v, 'must be an integer')
