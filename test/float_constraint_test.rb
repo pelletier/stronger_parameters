314ffca2b2016e09e9c200bf5a91be7df470c812
Thomas Pelletier <pelletier.thomas@gmail.com>
2017-02-03 15:20:17 -0800

    Use parenthesis for rejects and permits in tests

--- test/float_constraint_test.rb
+++ test/float_constraint_test.rb
@@ -3,17 +3,17 @@ require_relative 'test_helper'
 describe 'float parameter constraints' do
   subject { ActionController::Parameters.float }
 
-  permits "1.2", as: 1.2
-  permits "-1.2", as: -1.2
-  permits 1.2
-  permits -1.2
+  permits("1.2", as: 1.2)
+  permits("-1.2", as: -1.2)
+  permits(1.2)
+  permits(-1.2)
 
-  rejects 1
-  rejects '1'
-  rejects '-1'
-  rejects 'abc'
-  rejects '1.2.3'
-  rejects '.3'
-  rejects '3.'
-  rejects true
+  rejects(1)
+  rejects('1')
+  rejects('-1')
+  rejects('abc')
+  rejects('1.2.3')
+  rejects('.3')
+  rejects('3.')
+  rejects(true)
 end
